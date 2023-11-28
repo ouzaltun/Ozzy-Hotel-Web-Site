d147d3391879a6a9abffeb8e296e5a477c69137e
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-11-28 17:02:29 +0300

    Next.js kurulumu gerekli packagelar
    Next kurulumu gerekli packagelar

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Image from "next/image";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <Header></Header>
+      <Hero></Hero>
+      <Footer></Footer>
+    </main>
+  );
+}
