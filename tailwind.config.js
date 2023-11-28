217ccff49adeb5fe8de5e478ca1980ad550417e2
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-11-28 17:08:45 +0300

    Body css ayarlamaları
    Body css ayarlamaları

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        hero: "url(/hero/bg.png)",
+        hero2: "url(/hero/bg-2.png)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
